ef24470de234a407e6f495d9a8745a58abfc2f20
phorn3 <pius.horn@hs-offenburg.de>
2026-03-19 16:43:50 +0100

    Rename evaluators.py to llm_judge.py

--- src/pdf_parse_bench/eval/__init__.py
+++ src/pdf_parse_bench/eval/__init__.py
@@ -1,4 +1,4 @@
-from .evaluators import run_batch_evaluation, EvalPaths, save_results, FormulaResult, TableResult, LLMScore
+from .llm_judge import run_batch_evaluation, EvalPaths, save_results, FormulaResult, TableResult, LLMScore
 
 __all__ = [
     "run_batch_evaluation",
